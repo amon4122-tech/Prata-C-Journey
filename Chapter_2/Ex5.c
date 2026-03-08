86bfe011f7f1ffda2a561e513c4675e8050c2ed9
synthyentzer <amon4122@gmail.com>
2026-03-09 01:27:16 +0700

    Update Ex5.c

--- Chapter_2/Ex5.c
+++ Chapter_2/Ex5.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-float years;
-
 int main(void){
+    float number;
     printf("Введите количество лет: ");
     scanf("%f", &years);
     printf("Это %f секунд\n", years * 3.156e7);
