00d01e8580d4ffce6589c99949040f619f0d5eca
synthyentzer <amon4122@gmail.com>
2026-03-09 01:28:01 +0700

    Update Ex7.c

--- Chapter_2/Ex7.c
+++ Chapter_2/Ex7.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-float centimeters;
-
 int main(void){
+    float centimeters;
     printf("Введите значение в сантиметрах: ");
     scanf("%f", &centimeters);
     printf("Это %f дюймов\n", centimeters * 0.3937);
