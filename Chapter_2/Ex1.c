7b08a792e99ae0a239749cf1765d11fada2768a1
synthyentzer <amon4122@gmail.com>
2026-03-22 21:56:52 +0700

    Update Ex1.c

--- Chapter_2/Ex1.c
+++ Chapter_2/Ex1.c
@@ -13,15 +13,15 @@ long long h = LLONG_MIN;
 unsigned long long i = ULLONG_MAX;
 
 int main(void){
-    printf("Максимальное значение типа int: %d. Переполнение: %d\n", a, a + 1);
-    printf("Минимальное значение типа int: %d. Переполнение: %d\n", b, b - 1);
-    printf("Максимальное значение типа unsigned int: %u. Переполнение: %u\n", c, c + 1);
-    printf("Максимальное значение типа long: %ld. Переполнение: %ld\n", d, d + 1);
-    printf("Минимальное значение типа long: %ld. Переполнение: %ld\n", e, e - 1);
-    printf("Максимальное значение типа unsigned long: %lu. Переполнение: %lu\n", f, f + 1);
-    printf("Максимальное значение типа long long: %lld. Переполнение: %lld\n", g, g + 1);
-    printf("Минимальное значение типа long long: %lld. Переполнение: %lld\n", h, h - 1);
-    printf("Максимальное значение типа unsigned long long: %llu. Переполнение: %llu\n", i, i + 1);
+    printf("Maximum value of type int: %d. Overflow: %d\n", a, a + 1);
+    printf("Minimum value of type int: %d. Overflow: %d\n", b, b - 1);
+    printf("Maximum value of type unsigned int: %u. Overflow: %u\n", c, c + 1);
+    printf("Maximum value of type long: %ld. Overflow: %ld\n", d, d + 1);
+    printf("Minimum value of type long: %ld. Overflow: %ld\n", e, e - 1);
+    printf("Maximum value of type unsigned long: %lu. Overflow: %lu\n", f, f + 1);
+    printf("Maximum value of type long long: %lld. Overflow: %lld\n", g, g + 1);
+    printf("Minimum value of type long long: %lld. Overflow: %lld\n", h, h - 1);
+    printf("Maximum value of type unsigned long long: %llu. Overflow: %llu\n", i, i + 1);
 
     return 0;
 }
