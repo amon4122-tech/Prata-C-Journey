7d4ae71497ff22ac0ba23537c472d75fa335c3d5
synthyentzer <amon4122@gmail.com>
2026-03-31 20:44:05 +0700

    Update Ex8.c

--- Chapter_5/Ex8.c
+++ Chapter_5/Ex8.c
@@ -2,12 +2,12 @@
 
 int main (void){
     double first, second;
-    printf("Enter first and second number with dot:");
+    printf("Enter two numbers with dot (or q for exit):");
     while (scanf("%lf %lf", &first, &second) == 2) {
         double result = (first - second)/(first*second);
         printf("Result = %lf",result);
         printf("\n");
-        printf("Enter next two numbers with dot:");
+        printf("Enter next two numbers with dot (or q for exit):");
     }
     
     return 0;
