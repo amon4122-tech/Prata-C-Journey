a216995ace95743f144ddd9e19777fcdb27100d3
synthyentzer <amon4122@gmail.com>
2026-04-01 19:33:23 +0700

    Update Ex10.c

--- Chapter_5/Ex10.c
+++ Chapter_5/Ex10.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void square_sum(int lower_limit, int upper_limit);
+long long square_sum(int lower_limit, int upper_limit);
 
 int main(void) {
     int lower_limit, upper_limit;
-    printf("Enter lower limit:");
-    scanf("%d",&lower_limit);
-    printf("\n");
-    printf("Enter upper limit:");
-    scanf("%d",&upper_limit);
-    printf("\n");
+    printf("Enter lower and upper limits:");
+    while (scanf("%d %d",&lower_limit, &upper_limit) == 2) {
+        if (lower_limit >= upper_limit) {
+            printf("The lower limit cannot exceed or be equal to the upper limit!\n");
+            return 1;
+        }
+        long long result = square_sum(lower_limit, upper_limit);
+        printf("Square sum of two limits %d and %d = %lld \n", lower_limit, upper_limit, result);
+        printf("Enter next two limits:"); 
+    }
 
-    printf("Limits: %d %d", lower_limit, upper_limit);
-    printf("\n");
-    
-    system ("PAUSE");
     return 0;
 }
 
-void square_sum(int lower_limit, int upper_limit){
+long long square_sum(int lower_limit, int upper_limit){
+    long long sum_square = 0;
     for(int i = lower_limit; i <= upper_limit; i++){
-        
+        sum_square = sum_square + ((long long)i*i);
     }
+    return sum_square;
 }
