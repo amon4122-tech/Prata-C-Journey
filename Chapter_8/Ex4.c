0ea4f54280863d2b276aa4024b2020ea130a6e47
synthyentzer <amon4122@gmail.com>
2026-04-14 20:46:00 +0700

    Add harmonic number calculation program

---
+++ Chapter_8/Ex4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+double harmonic (double, double);
+
+int main (void){
+    double a,b;
+    double harmonic_num;
+    printf("Enter two numbers for callculate of harmonic number:");
+    while (scanf("%lf %lf",&a, &b) == 2) {
+        harmonic_num = harmonic(a, b);
+        printf("Harmonic number of %g and %g = %.3lf", a, b, harmonic_num);
+        putchar('\n');
+        printf("Enter next two numbers for callculate of harmonic number:\n");
+    }
+}
+
+double harmonic (double i, double j){
+    if (i + j == 0) {
+        printf("Error: sum of numbers must not be equal to 0! ");
+        return 0;
+    }
+    double result = (2.0 * i * j) / (i +j);
+    return result; 
+}
