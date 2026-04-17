f6228fde1276dcd3e60f9c1793f5fc638f8dad4d
synthyentzer <amon4122@gmail.com>
2026-04-17 09:59:41 +0700

    Update Ex6.c

--- Chapter_8/Ex6.c
+++ Chapter_8/Ex6.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
 
-void sort_numbers(double *, double *, double *);
+void sort_number(double *, double *, double * );
 
 int main (void){
     double a,b,c;
-    printf("Enter three number (or q for quit):");
-    while (scanf("%lf %lf %lf", &a, &b, &c) == 3) {
-        sort_numbers(&a, &b, &c);
-        printf("Enter new three number (or q for quit):");
+    printf( "Enter number for sort:");
+    while(scanf("%lf %lf %lf", &a, &b, &c)==3){
+        sort_number(&a, &b, &c);
+        printf("Sorted number = %g %g %g", a,b,c);
     }
-    printf("Goodbye!\n");
-    return 0;
-
 }
 
-void sort_numbers(double *x, double *y, double *z) {
+void sort_number(double *x, double *y, double *z){
     double temp;
-
-    if (*x > *y) { 
-        temp = *x; 
-        *x = *y; 
-        *y = temp; 
-    }
-    if (*x > *z) { 
-        temp = *x; 
-        *x = *z; 
-        *z = temp; 
-    }
-    if (*y > *z) { 
-        temp = *y; 
-        *y = *z; 
-        *z = temp; 
-    }
-
-    printf("Sorted numbers: %g %g %g!!!\n", *x, *y, *z);
+    if (*x > *y){temp = *x; *x = *y; *y = temp; }
+    if (*x > *z){temp = *z; *x = *z; *z = temp; }
+    if (*y > *z){temp = *z; *z = *y; *y = temp; }
 }
